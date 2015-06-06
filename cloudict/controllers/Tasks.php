67ea4e0d1ad0b7b4150f7374f87992bb12ce0d19
Milos Zivkovic <developer.milos.zivkovic@gmail.com>
2015-06-06 07:49:36 +0200

    Created Tasks controller

---
+++ cloudict/controllers/Tasks.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tasks extends CI_Controller {
+
+    /**
+     * THIS FILE SHOULD BE EDITED ONLY FROM tasks-feature BRANCH
+     * git checkout tasks-feature
+     */
+
+    /**
+     * None of these methods are final
+     * this is a template for RESTful web service
+     */
+
+    /**
+     * Shows all Tasks for current user
+     */
+    public function index(){
+
+    }
+
+    /**
+     * shows view for creating new task
+     */
+    public function create(){
+
+    }
+
+    /**
+     * Target for create method, stores created task in database
+     */
+    public function store(){
+
+    }
+
+    /**
+     * Shows current task, i.e no. of people who finished the selected task
+     * TODO: Find a way to limit the access to task to people who are on the task
+     */
+    public function show(){
+        
+    }
+
+    /**
+     * Show view for editing selected task
+     */
+    public function edit(){
+
+    }
+
+    /**
+     * Method for updating selected task
+     */
+    public function update(){
+
+    }
+
+    /**
+     * Method for deleting selected tasks
+     */
+    public function destroy(){
+
+    }
+}
