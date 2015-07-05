ee4fe0a449a2edcedf9e8dae34f0303a8ac691c3
Devel <developer.milos.zivkovic@gmail.com>
2015-07-05 12:47:57 +0200

    Changed connection parameters for Database

--- cloudict/config/database.php
+++ cloudict/config/database.php
@@ -64,7 +64,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.2.102',
 	'username' => 'ict',
 	'password' => 'cloud',
 	'database' => 'ictcloud',
