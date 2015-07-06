4bcdb5c95e2833860398775c5ded08db03fbd26b
Vincanac <petrovic-goran@outlook.com>
2015-07-06 12:33:41 +0200

    Changed welcome view to login, added form for loging in

--- cloudict/views/login.php
+++ cloudict/views/login.php
@@ -71,15 +71,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+        <form action="<?php echo base_url('user/login')?>" method="post">
+            <input type="text" name="user">
+            <input type="password" name="password">
+            <input type="submit" name="login" value="Login">
+        </form>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
