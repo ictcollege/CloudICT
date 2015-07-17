7905593ace08eee6152f113a0bf8b0a2af5a0a8e
Vincanac <petrovic-goran@outlook.com>
2015-07-17 11:28:21 +0200

    User controller, copied controller from login branch, edite login function, added part for inserting data into uselog table,

--- cloudict/controllers/User.php
+++ cloudict/controllers/User.php
@@ -14,40 +14,7 @@ class User extends CI_Controller {
     public function index(){
         //helpers
         $this->load->helper('url');
-        $this->load->helper('form');
 
-        //variables
-        $base_url = base_url();
-
-        //form elements
-        $form_attr = array(
-            "id" => "formLogin",
-            "role" => "form"
-        );
-
-        $username_attr = array(
-            "class" => "form-control tbUsername" ,
-            "placeholder" => "Username" ,
-            "name" => "username" ,
-            "autofocus" => "autofocus"
-        );
-
-        $password_attr = array(
-            "class" => "form-control tbPassword" ,
-            "placeholder" => "Password" ,
-            "name" => "password"
-        );
-
-        //data to view
-        $data['base_url']= $base_url;
-        $data['title'] = "ICT Cloud | Login";
-        //data for form
-        $data['form_attr'] = $form_attr;
-        $data['username_attr'] = $username_attr;
-        $data['password_attr'] = $password_attr;
-
-        //views
-        $this->load->view('header', $data);
         $this->load->view('login');
     }
 
@@ -57,6 +24,8 @@ class User extends CI_Controller {
         $password = $this->input->post('Password');
 
         $this->load->model('UserModel');
+        $this->load->model('UserLogModel');
+        $this->load->helper('url');
 
         $user = $this->UserModel->getUser($username, md5($password));
 
@@ -64,18 +33,24 @@ class User extends CI_Controller {
         {
             $idrole = $user["User"]["IdRole"];
             $username = $user["User"]["User"];
+            $id = $user["User"]["Id"];
 
             $groups = $this->UserModel->getAllUsersInGroups($user["User"]["Id"]);
 
             $session= array(
                 'username' => $username,
                 'role' => $idrole,
-                'group' => $groups
+                'group' => $groups,
+                'id' => $id
             );
 
             $this->session->set_userdata($session);
+
+            $this->UserLogModel->login($user["User"]["Id"]);
+
+            redirect('/chat');
         }
 
-        echo json_encode($session);
+        redirect('/');
     }
 }
