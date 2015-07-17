56d837e77a501eb38a3fbb057ede08d431d46612
Vincanac <petrovic-goran@outlook.com>
2015-07-17 11:24:17 +0200

    messages html, this part load with ajax

--- cloudict/views/messages.php
+++ cloudict/views/messages.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Goran
- * Date: 7/6/2015
- * Time: 1:46 PM
- */
+<ul class="media-list">
+
+    <li class="media">
+
+        <div class="media-body">
+            <?php foreach($Messages as $Message){?>
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object img-circle " src="<?php echo base_url();?>public/img/user.png" />
+                </a>
+                <div class="media-body" >
+                    <?php echo $Message['Message']; ?>
+                    <br />
+                    <small class="text-muted"> <?php echo $Message['Sender']; ?> |  <?php echo date('H:i d M Y',$Message['Time']);?></small>
+                    <hr />
+                </div>
+            </div>
+            <?php } ?>
+
+        </div>
+    </li>
+
+</ul>
