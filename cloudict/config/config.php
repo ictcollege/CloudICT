46ecb4ac7ad4469ac6dcc118fd08a67679ee26b3
Devel <develper.milos.zivkovic@gmail.com>
2016-02-09 18:32:21 +0100

    Configurtion setup

--- cloudict/config/config.php
+++ cloudict/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/cloud_project/';
+$config['base_url'] = 'http://192.168.2.101/cloud_project/';
 
 /*
 |--------------------------------------------------------------------------
